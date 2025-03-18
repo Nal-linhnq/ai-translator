f7dcd0d4e1a68d9645232372b717da18da8cdae5
Linh Nguyen Quang <linhnq@nal.vn>
2025-03-19 06:14:14 +0700

    Update style preview

--- src/components/MarkdownRenderer.tsx
+++ src/components/MarkdownRenderer.tsx
@@ -5,14 +5,14 @@ import remarkGfm from "remark-gfm";
 
 const MarkdownRenderer: React.FC<{ content: string }> = ({ content }) => {
   return (
-    <div className="prose max-w-none">
+    <div className="prose prose-sm md:prose-lg lg:prose-xl break-words max-w-full">
       <ReactMarkdown
         remarkPlugins={[remarkGfm, remarkEmoji]}
         components={{
           a: ({ ...props }) => (
             <a
               {...props}
-              className="text-blue-600 underline hover:text-blue-800"
+              className="text-blue-600 underline break-all hover:text-blue-800"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -23,14 +23,14 @@ const MarkdownRenderer: React.FC<{ content: string }> = ({ content }) => {
           ol: ({ ...props }) => <ol className="list-decimal pl-5" {...props} />, // Ordered List
           blockquote: ({ ...props }) => (
             <blockquote
-              className="border-l-4 border-gray-400 pl-4 italic"
+              className="border-l-4 border-gray-400 pl-4 italic break-words"
               {...props}
             />
           ),
           code({ children, ...props }) {
             return (
               <code
-                className="bg-gray-50 border border-gray-300 text-red-500 px-1 py-1 rounded text-sm"
+                className="bg-gray-50   text-red-500 px-1 py-1 rounded text-sm  whitespace-break-spaces break-words"
                 {...props}
               >
                 {children}
