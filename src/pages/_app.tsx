c14cb047d9e53c543535af8c8623a4b36b72bf19
Linh Nguyen Quang <linhnq@nal.vn>
2025-03-21 09:01:40 +0700

    Add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,15 @@ import "@/styles/globals.css";
 import "@/lib/i18n";
 import { appWithTranslation } from "next-i18next";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default appWithTranslation(App);
